aa7db617613ec1a89948472a87bf69655bda1a36
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-25 09:55:13 +0530

    [chinki] Creat Contact details

---
+++ Address_book/Address_book/CreatContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_book
+{
+    internal class CreatContact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int PhoneNo { get; set; }
+        public string Email { get; set; }
+    }
+}
